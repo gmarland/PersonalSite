92c2f18713d7f281e7a5d373c068c8e9478cc581
Gareth <gareth.marland@gmail.com>
2017-04-04 19:00:45 -0400

    fixing

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
 
 // Create the http server
 	
-	var http = require("http").createServer(app).listen((process.env.PORT || 8080));
+	var http = require("http").createServer(app).listen(8080);
 
 // Removing www from URL routes
 
