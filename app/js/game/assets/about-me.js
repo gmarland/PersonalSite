211dda0cbbc29a97226eba6816a7c4b547e98fa1
Gareth Marland <gareth.marland@gmail.com>
2019-03-24 21:49:27 +0000

    updating

--- app/js/game/assets/about-me.js
+++ app/js/game/assets/about-me.js
@@ -12,7 +12,7 @@ angular.module("gm.game.assets.aboutme", []).service("$aboutme", [function() {
 			aboutMeObject.getContent = function() {
 				return {
 					title: "About Me",
-					content: "<p>My name is Gareth Marland. I'm a British programmer who currently lives in Toronto, Canada with my lovely Canadian wife.</p><p>By day, I work as the Senior Developer on Clarity for Rand Worldwide, a product that helps manage Autodesk Revit. It’s wrote in C# using the MVC.Net framework and is a constantly evolving challenge.</p><p>By night, I am one of the co-founders of BoardThing which is an online collaborative workspace. I also created a collaborative brainstroming and voting website called Dotstorming which is used in classrooms all over the world.</p>"
+					content: "<p>My name is Gareth Marland. I'm a senior full stack software developer who creates professional business applications.</p><p>I've worked all over the world building software and I'm driven to produce quality solutions and passionate about software development.</p><p>By day, I work as the Senior Developer on Clarity for Rand Worldwide, a product that helps manage Autodesk Revit. It’s wrote in C# using the MVC.Net framework and is a constantly evolving challenge.</p><p>By night, I am one of the co-founders of BoardThing which is an online collaborative workspace. I also created a collaborative brainstroming and voting website called Dotstorming which is used in classrooms all over the world.</p>"
 				};
 			};
 
